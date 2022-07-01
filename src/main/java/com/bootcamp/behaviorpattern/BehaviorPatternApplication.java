a978668a57f710376c31309eab775e625528f4cf
jnrch41 <jonathanrojas31@gmail.com>
2022-06-30 22:11:33 -0300

    first commit

---
+++ src/main/java/com/bootcamp/behaviorpattern/BehaviorPatternApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.behaviorpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BehaviorPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BehaviorPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/behaviorpattern/BehaviorPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.behaviorpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BehaviorPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
